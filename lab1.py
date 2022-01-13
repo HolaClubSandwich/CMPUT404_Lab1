09c433fcc1c51ccc6f1257f5c9a1603cd284e5ae
HolaClubSandwich <junyancan@gmail.com>
2022-01-12 17:10:56 -0800

    lab1 done

--- lab1.py
+++ lab1.py
@@ -4,3 +4,8 @@ print("Version of requests is ", requests.__version__)
 
 homepage = requests.get("https://www.google.com")
 
+raw_url = "https://raw.githubusercontent.com/HolaClubSandwich/CMPUT404_Lab1/master/lab1.py"
+
+code = requests.get(raw_url, stream=True)
+
+print(code.text)
